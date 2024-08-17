310b5048e0bdfe0ab6366160601818ff12a9cea3
Bartas Dausynas <bartas3@gmail.com>
2024-08-17 19:07:10 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import style from "./App.module.css";
+import bkg from "./img/wallpaper.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+      <img className={style.bkg} src={bkg} />
+      <div className={style.glass}>
+        <h1 className={style.title}>HANGMAN</h1>
+        <div className={style.game}>
+          {/* <Tab />
+          <Hangman /> */}
+        </div>
+        <div className={style.footer}>HANGMAN</div>
+      </div>
     </div>
   );
 }
